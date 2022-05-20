22127cf23eb6d7a9bb0498db4b21947769d9c4df
Claudia Brown <claudiabrown297@gmail.com>
2022-05-19 21:29:45 -0400

    updated npm then added html content with CSS

--- src/App.js
+++ src/App.js
@@ -6,14 +6,13 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1>
+        <Weather />
         <footer>
           This project was coded by{" "}
           <a href="https://fabulous-kheer-cd4c48.netlify.app/" target="_blank" rel="noopener noreferrer">Claudia Brown</a>
           {" "}and is{" "}
           <a href="https://github.com/cbrown297/weather-react" target="_blank" rel='noopener noreferrer'>open-sourced on GitHub</a>
         </footer>
-        <Weather />
       </div>
     </div>
   );
--- src/Weather.js
+++ src/Weather.js
@@ -1,8 +1,69 @@
 import React, {useState} from "react";
 import axios from "axios";
+import './Weather.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Weather() {
-  const [city, setCity] = useState("");
+  return (
+  <div className="Weather">
+    <form className="search-engine">
+      <div className="row">
+        <div className="col-9">
+          <input 
+            type="search" 
+            placeholder="Enter a city..." 
+            className="form-control"
+          />
+        </div>
+        <div className="col-3">
+          <input 
+            type="Submit" 
+            value="Search" 
+            className="btn btn-primary w-100"
+          />
+        </div>
+      </div>
+    </form>
+    <h1>New York</h1>
+    <ul>
+      <li>Wednesday 7:00</li>
+      <li>Mostly Cloudy</li>
+    </ul>
+    <div className="row mt-3">
+      <div className="col-6">
+        <div className="clearfix">
+          <img 
+          src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" 
+          alt="Mostly Cloudy"
+          className="float-left"
+          />
+          <span className="float-left">
+            <span className="temperature">6</span>
+            <span className="unit">°C</span>  
+          </span>
+        </div>
+      </div>
+      <div className="col-6">
+        <ul>
+          <li>Precipitation: 15%</li>
+          <li>Humidity: 72%</li>
+          <li>Wind: 3mph</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  );
+}
+
+
+
+
+
+
+
+
+
+  /* const [city, setCity] = useState("");
   const [weather, setWeather] = useState(null);
   const [loaded, setLoaded] = useState(false);
 
@@ -30,7 +91,7 @@ export default function Weather() {
   }
 
   let form = (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} className="search-engine">
       <input type="search" placeholder="Enter a city" onChange={updateCity} />
       <button type="Submit">Search</button>
     </form>
@@ -53,5 +114,4 @@ export default function Weather() {
     );
   } else {
     return form;
-  }
-}
+  }*/
